ec26c326a57758ce11f68724edc220600b6c524a
truthixify <truthixify@gmail.com>
2025-04-02 23:42:04 +0100

    added polynomials crate

---
+++ polynomials/src/lib.rs
@@ -0,0 +1,17 @@
+struct UnivariatePolynomial {}
+
+impl UnivariatePolynomial {
+    fn random() -> Self {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert!(true)
+    }
+}
