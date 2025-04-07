fb4ca9aadc1eaede6806cd76a6df09a3be736926
truthixify <truthixify@gmail.com>
2025-04-07 23:37:12 +0100

    added benchmark for dense multilinear polynomial

--- polynomials/benches/criterion.rs
+++ polynomials/benches/criterion.rs
@@ -2,14 +2,17 @@ use criterion::{Criterion, criterion_group, criterion_main};
 mod multilinear_benchmarks;
 mod univariate_benchmarks;
 
-use multilinear_benchmarks::sparse::sparse_multilinear_polynomial_benchmarks;
+use multilinear_benchmarks::{
+    dense::dense_multilinear_polynomial_benchmarks,
+    sparse::sparse_multilinear_polynomial_benchmarks,
+};
 use univariate_benchmarks::{
     dense::dense_univariate_polynomial_benchmarks, sparse::sparse_univariate_polynomial_benchmarks,
 };
 
 criterion_group!(
     name = polynomials;
-    config = Criterion::default().sample_size(100).configure_from_args();
-    targets = sparse_multilinear_polynomial_benchmarks, dense_univariate_polynomial_benchmarks, sparse_univariate_polynomial_benchmarks
+    config = Criterion::default().sample_size(10).configure_from_args();
+    targets = dense_multilinear_polynomial_benchmarks, sparse_multilinear_polynomial_benchmarks, dense_univariate_polynomial_benchmarks, sparse_univariate_polynomial_benchmarks
 );
 criterion_main!(polynomials);
--- polynomials/benches/multilinear_benchmarks/mod.rs
+++ polynomials/benches/multilinear_benchmarks/mod.rs
@@ -1,1 +1,2 @@
+pub mod dense;
 pub mod sparse;
